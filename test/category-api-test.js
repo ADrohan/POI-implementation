59501caa01b0061e9aa44984ba5e91924af2e966
aileendrohan <aileendrohan@gmail.com>
2021-05-10 12:14:59 +0100

    installed http client axios for first category tests

--- test/category-api-test.js
+++ test/category-api-test.js
@@ -1,9 +1,25 @@
 "use strict";
 
 const assert = require("chai").assert;
+const axios = require("axios");
 
 suite("Category API tests", function () {
-  test("get categories", function () {
-    assert.equal(1, 1);
+  test("get categories", async function () {
+    const response = await axios.get("http://localhost:3000/api/categories");
+    const categories = response.data;
+    assert.equal(4, categories.length);
+   // console.log(response.data);
+    //assert.equal(1, 1);
+  });
+  test("get one category", async function () {
+    let response = await axios.get("http://localhost:3000/api/categories");
+    const categories = response.data;
+    assert.equal(4, categories.length);
+
+    const oneCategoryUrl = "http://localhost:3000/api/categories/" + categories[0]._id;
+    response = await axios.get(oneCategoryUrl);
+    const oneCategory = response.data;
+
+    assert.equal(oneCategory.name, "urban");
   });
 });
