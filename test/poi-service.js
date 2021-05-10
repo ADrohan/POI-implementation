a135865867a627699d0d125429c1550b78f3be03
aileendrohan <aileendrohan@gmail.com>
2021-05-10 15:43:16 +0100

    full suite of user API tests implemented

--- test/poi-service.js
+++ test/poi-service.js
@@ -54,6 +54,52 @@ class PoiService {
     const response = await axios.post(this.baseUrl + "/api/users", newUser);
     return response.data;
   }
+
+  async getUsers() {
+    try {
+      const response = await axios.get(this.baseUrl + "/api/users");
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async getUser(id) {
+    try {
+      const response = await axios.get(this.baseUrl + "/api/users/" + id);
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async createUser(newUser) {
+    try {
+      const response = await axios.post(this.baseUrl + "/api/users", newUser);
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async deleteAllUsers() {
+    try {
+      const response = await axios.delete(this.baseUrl + "/api/users");
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async deleteOneUser(id) {
+    try {
+      const response = await axios.delete(this.baseUrl + "/api/users/" + id);
+      return response.data;
+    } catch (e) {
+      return null;
+    }
+  }
+
 }
 
 module.exports = PoiService;
