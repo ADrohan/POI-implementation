4e73608c0c2bcdde7692617318d5c11639f0cefd
aileendrohan <aileendrohan@gmail.com>
2021-05-10 13:17:59 +0100

    basic category test working

--- test/category-api-test.js
+++ test/category-api-test.js
@@ -3,14 +3,14 @@
 const assert = require("chai").assert;
 const axios = require("axios");
 
+
 suite("Category API tests", function () {
   test("get categories", async function () {
     const response = await axios.get("http://localhost:3000/api/categories");
     const categories = response.data;
     assert.equal(4, categories.length);
-   // console.log(response.data);
-    //assert.equal(1, 1);
   });
+
   test("get one category", async function () {
     let response = await axios.get("http://localhost:3000/api/categories");
     const categories = response.data;
@@ -22,4 +22,48 @@ suite("Category API tests", function () {
 
     assert.equal(oneCategory.name, "urban");
   });
+
+  test("create a category", async function () {
+    const categoriesUrl = "http://localhost:3000/api/categories";
+    const newCategory = {
+      name: "lakes",
+    };
+
+    const response = await axios.post(categoriesUrl, newCategory);
+    const returnedCategory = response.data;
+    assert.equal(201, response.status);
+
+    assert.equal(returnedCategory.name, "lakes");
+  });
+
+  test("delete a category", async function () {
+    let response = await axios.get("http://localhost:3000/api/categories");
+    let categories = response.data;
+    const originalSize = categories.length;
+
+    const oneCategoryUrl = "http://localhost:3000/api/categories/" + categories[0]._id;
+    response = await axios.get(oneCategoryUrl);
+    const oneCategory = response.data;
+    assert.equal(oneCategory.name, "urban");
+
+    response = await axios.delete("http://localhost:3000/api/categories/" + categories[0]._id);
+    assert.equal(response.data.success, true);
+
+    response = await axios.get("http://localhost:3000/api/categories");
+    categories = response.data;
+    assert.equal(categories.length, originalSize - 1);
+  });
+
+  test("delete all categories", async function () {
+    let response = await axios.get("http://localhost:3000/api/categories");
+    let categories = response.data;
+    const originalSize = categories.length;
+    assert(originalSize > 0);
+    response = await axios.delete("http://localhost:3000/api/categories");
+    response = await axios.get("http://localhost:3000/api/categories");
+    categories = response.data;
+    assert.equal(categories.length, 0);
+  });
+
 });
+
